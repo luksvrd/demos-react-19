308ea6cbb23ba5a5c38e877cf308b914bfa27448
luksvrd <ljvy9b@gmail.com>
2023-03-02 21:45:09 -0600

    implement fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import CatCard from "./components/CatCard";
 import Header from "./components/Header";
 // Using a bundler so we dont need to put the file extension in the import statement.
 import catsData from "./data/cats";
+// Fragment is a specifically named component of React that doesn't render anything to the DOM. It's just a way to wrap multiple elements without having to use a div.
 console.log(catsData);
 
 function App() {
   // calling a function inside another fuction
-  return <Header text="Hello World!" />;
+  return (
+    // <> is a short syntactic sugar for React.Fragment
+    <>
+      <Header text="Hello Kitty" />
+      <CatCard cat={catsData[0]} />
+    </>
+  );
 }
 
 export default App;
