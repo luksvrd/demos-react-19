2bb3b83c2829e1b28541f8024aa2b0478f6d389e
luksvrd <ljvy9b@gmail.com>
2023-03-02 20:59:49 -0600

    import cats data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header";
 // Using a bundler so we dont need to put the file extension in the import statement.
+import catsData from "./data/cats";
+console.log(catsData);
 
 function App() {
   // calling a function inside another fuction
