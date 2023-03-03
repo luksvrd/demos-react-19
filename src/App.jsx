7c2786233feea128b0b18ea6ddc1743a2aca2dea
luksvrd <ljvy9b@gmail.com>
2023-03-02 20:54:17 -0600

    pass prop text from parent to child

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Header from "./components/Header";
 // Using a bundler so we dont need to put the file extension in the import statement.
 
 function App() {
-  return <Header />;
+  // calling a function inside another fuction
+  return <Header text="Hello World!" />;
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 // when you actually use the component is when the capitalization matters. So, if you're using the component in a JSX file, then you need to use the capitalization. If you're using the component in a JavaScript file, then you don't need to use the capitalization. So, if you're using the component in a JSX file, then you need to use the capitalization. If you're using the component in a JavaScript file, then you don't need to use the capitalization.
-export default function Header() {
-    return ,h1 className="text-3xl font-bold underline">Hello world!</h1>;
+export default function Header({ text }) {
+  // Props are passed as an objet. we can destructure it to get the text prop.
+  return <h1 className="text-3xl font-bold underline">{text}</h1>;
 }
