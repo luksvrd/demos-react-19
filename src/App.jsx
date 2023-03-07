d268bcf57faec3116344cd4dd21ec1ba866f51c4
luksvrd <ljvy9b@gmail.com>
2023-03-06 20:58:19 -0600

    sloppy commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+// Using a bundler so we dont need to put the file extension in the import statement.
 import CatCard from "./components/CatCard";
 import Header from "./components/Header";
-// Using a bundler so we dont need to put the file extension in the import statement.
-import GroceryLI from "./components/groceryLI";
+import NavBar from "./components/NavBar";
 import UserCard from "./components/UserCard";
+import GroceryLI from "./components/groceryLI";
 import catsData from "./data/cats";
 import groceriesData from "./data/groceries";
 import userData from "./data/users";
-import NavBar from "./components/NavBar";
 // Fragment is a specifically named component of React that doesn't render anything to the DOM. It's just a way to wrap multiple elements without having to use a div.
-console.log(catsData);
 
 function App() {
   // calling a function inside another fuction
@@ -24,11 +23,6 @@ function App() {
       })}
 
       {/* TODO: use a 'map' to create a list for each grocery item. */}
-
-      {/* {groceries.map((grocery) => {
-        return <li key={grocery.id}>{grocery.name}</li>;
-      })} */}
-
       {/* TODO: nexxt to each one, render a checkbox. Check it off if purchased =ture. Hint (conditional rendering) */}
       <ul>
         {groceriesData.map((grocery) => {
@@ -50,5 +44,7 @@ export default App;
 // the map() method is a declarative way to create a new array with the results of calling a provided function on every element in the calling array
 // able to apply a function to each object in an array
 // does not mutate the original array
+
 // the filter() method creates a new array with all elements that pass the test implemented by the provided function
+
 // the reduce() method executes a reducer function (that you provide) on each element of the array, resulting in a single output value
---
+++ src/components/Counter.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const stuff = useState();
+  console.log(stuff);
+
+  return <p>Counter Page</p>;
+}
