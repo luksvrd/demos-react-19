e6c09a35696351224d32723c56f5ecf6cfc3c097
luksvrd <ljvy9b@gmail.com>
2023-03-06 20:29:35 -0600

    add hard coded nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+export defaul function NavBar() {
+    return <nav>
+    <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/cats">Cats</a></li>
+    <li><a href="/users">Users</a></li>
+    <li><a href="/groceries">Groceries</a></li>
+    <li><a href="/counter">Counter</a></li>
+  </ul>
+</nav>
+}
